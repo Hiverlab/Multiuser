2b6785a52974cf474b5ede713b4aebe673b42df0
pzyu <pangzhengyu@live.com>
2019-09-27 16:03:58 +0800

    Add spectator view

--- Assets/Scripts/SpectatorManager.cs
+++ Assets/Scripts/SpectatorManager.cs
@@ -1,18 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class SpectatorManager : MonoBehaviour
 {
+    [SerializeField]
+    private bool isSpectatorActive = false;
+
+    [SerializeField]
+    private GameObject ovrCameraRig;
+
+    [SerializeField]
+    private Transform spectatorCamera;
+
+    [SerializeField]
+    private List<Transform> cameraPositionList;
+
+    [SerializeField]
+    private float delayBetweenViews = 5.0f;
+
+    private int cameraPositionIndex;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (isSpectatorActive) {
+            Initialize();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Initialize() {
+        ovrCameraRig.SetActive(false);
+
+        cameraPositionIndex = 0;
+
+        StartCoroutine(SwitchCameraViewCoroutine());
+    }
+    
+    private IEnumerator SwitchCameraViewCoroutine() {
+        spectatorCamera.DOMove(cameraPositionList[cameraPositionIndex].position, 1.0f);
+        spectatorCamera.DORotate(cameraPositionList[cameraPositionIndex].eulerAngles, 1.0f);
+
+        yield return new WaitForSeconds(delayBetweenViews);
+
+        cameraPositionIndex = (cameraPositionIndex + 1) % cameraPositionList.Count;
+
+        StartCoroutine(SwitchCameraViewCoroutine());
     }
 }
