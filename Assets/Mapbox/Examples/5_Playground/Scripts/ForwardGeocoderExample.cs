9db7b4e65b7cd3898891d3e22419a220e6be6041
pzyu <pangzhengyu@live.com>
2019-11-05 15:40:44 +0800

    Import mapbox

---
+++ Assets/Mapbox/Examples/5_Playground/Scripts/ForwardGeocoderExample.cs
@@ -0,0 +1,43 @@
+//-----------------------------------------------------------------------
+// <copyright file="ForwardGeocoderExample.cs" company="Mapbox">
+//     Copyright (c) 2016 Mapbox. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Mapbox.Examples.Playground
+{
+	using UnityEngine;
+	using UnityEngine.UI;
+	using Mapbox.Json;
+	using Mapbox.Utils.JsonConverters;
+	using Mapbox.Geocoding;
+
+	public class ForwardGeocoderExample : MonoBehaviour
+    {
+        [SerializeField]
+        ForwardGeocodeUserInput _searchLocation;
+
+        [SerializeField]
+        Text _resultsText;
+
+        void Awake()
+        {
+            _searchLocation.OnGeocoderResponse += SearchLocation_OnGeocoderResponse;
+        }
+
+        void OnDestroy()
+        {
+            if (_searchLocation != null)
+            {
+                _searchLocation.OnGeocoderResponse -= SearchLocation_OnGeocoderResponse;
+            }
+        }
+
+        void SearchLocation_OnGeocoderResponse(ForwardGeocodeResponse response)
+        {
+            _resultsText.text = JsonConvert.SerializeObject(_searchLocation.Response, Formatting.Indented, JsonConverters.Converters);
+
+            Debug.Log(_searchLocation.Response.Features[0].Geometry.Coordinates);
+        }
+    }
+}
